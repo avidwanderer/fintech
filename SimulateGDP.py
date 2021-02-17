decc02551c249d86dfcbf30cd27a3a2132fde20b
Matthew Blazewicz <42652295+avidwanderer@users.noreply.github.com>
2021-02-16 23:35:30 -0600

    Fixed errors associated with instantiation

--- SimulateGDP.py
+++ SimulateGDP.py
@@ -6,36 +6,40 @@ import random
 class economy():
     def __init__(self):
         #each increment of economyStateRemain is the equivalent of 1 quarter
-        economyStateRemain = 0  #economyStateRemain = remaining time for current economic state
-        economyStateLength = 16  #initial duration for current economic state
-        economyState = "Bull Market"  #economyState = "Depression", "Bear Market", "Bull Market", "Boomtime"
-        globalGDP = 87751541  #GDP starting point in $MM, see reference https://databank.worldbank.org/data/download/GDP.pdf
-        globalGDP_Growthrate = 0.0221  #initial annual growth rate
+        self.economyStateRemain = 15  #economyStateRemain = remaining time for current economic state
+        self.economyStateLength = 16  #initial duration for current economic state
+        self.economyState = "Bull Market"  #economyState = defines one of four potential economic states
+        self.globalGDP = 87751541  #GDP starting point in $MM, see reference https://databank.worldbank.org/data/download/GDP.pdf
+        self.globalGDPGrowthRate = 0.0221  #initial annual growth rate
+        self.rn1 = random.randint(1,100)
         
-        def simulate_GDP():
-            if economyStateRemain == 0:
-                rn1 = random.randint(1,100)
-                if rn1 < 10:
-                    economyState = "Depression"
-                elif rn1 < 50:
-                    economyState = "Bear Market"
-                elif rn1 < 90:
-                    economyState = "Bull Market"
-                elif rn1 <= 100:
-                    economyState = "Boomtime"
-                economyStateLength = random.randint(4,16)
-                economyStateRemain = economyStateLength
-            if economyState == "Depression":
-                rn2 = random.randint(1,100)
-                globalGDP_Growthrate = (0 - (random.randint(1,100)/100))
-            elif economyState == "Bear Market":
-                rn2 = random.randint(1,100)
-                globalGDP_Growthrate = (0 + (random.randint(1,100)/100))
-            elif economyState == "Bull Market":
-                rn2 = random.randint(1,100)
-                globalGDP_Growthrate = (1 + (random.randint(1,100)/100))
-            elif economyState == "Boomtime":
-                rn = random.random.randint(1,100)
-                globalGDP_Growthrate = (2 + (random.randint(1,100)/100))
-            globalGDP = globalGDP + globalGDP * globalGDP_Growthrate / 4 / 100 #divided by 4 to account for each call simulating 1 quarter
-            economyStateRemain -= 1
+    def simulateGDP(self):
+        self.globalGDP = self.globalGDP + self.globalGDP * self.globalGDPGrowthRate / 4 / 100 #divided by 4 to account for each call simulating 1 quarter
+        self.economyStateRemain = self.economyStateRemain - 1
+        
+        if self.economyStateRemain == 0:
+            self.rn1 = random.randint(1,100)
+            if self.rn1 < 10:
+                self.economyState = "Depression"
+            elif self.rn1 < 50:
+                self.economyState = "Bear Market"
+            elif self.rn1 < 90:
+                self.economyState = "Bull Market"
+            elif self.rn1 <= 100:
+                self.economyState = "Boomtime"
+            self.economyStateLength = random.randint(4,16)
+            self.economyStateRemain = self.economyStateLength
+        if self.economyState == "Depression":
+            self.rn2 = random.randint(1,100)
+            self.globalGDPGrowthRate = (0 - (random.randint(1,100)/100))
+        elif self.economyState == "Bear Market":
+            self.rn2 = random.randint(1,100)
+            self.globalGDPGrowthRate = (0 + (random.randint(1,100)/100))
+        elif self.economyState == "Bull Market":
+            self.rn2 = random.randint(1,100)
+            self.globalGDPGrowthRate = (1 + (random.randint(1,100)/100))
+        elif self.economyState == "Boomtime":
+            self.rn = random.randint(1,100)
+            self.globalGDPGrowthRate = (2 + (random.randint(1,100)/100))
+        
+
